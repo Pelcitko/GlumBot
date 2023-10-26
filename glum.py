187352cf378bda254db0759fa1bda7067461b549
lpelc <lukas.pelc@ceskatelevize.cz>
2023-10-26 16:43:59 +0200

    Add loading and saveing history

--- glum.py
+++ glum.py
@@ -1,10 +1,14 @@
 import openai
 import logging
+from colorama import Fore, Style, init
 from config import OPENAI_API_KEY
 
 openai.api_key = OPENAI_API_KEY
 
 def get_response(messages):
+    # logging.debug(f'Sending: {messages}')
+    print(f'{Fore.LIGHTBLACK_EX}Sending: {messages}{Style.RESET_ALL}\n')
+    
     try:
         # Volání OpenAI API
         response = openai.ChatCompletion.create(
@@ -13,6 +17,10 @@ def get_response(messages):
             temperature=0.7,
             max_tokens=128
         )
+        
+        # logging.debug(f'Received: {response}')
+        print(f'{Fore.LIGHTBLACK_EX}Received: {response}{Style.RESET_ALL}\n')
+
         message = response['choices'][0]['message']['content'].strip()
         return message
     except Exception as e:
--- main.py
+++ main.py
@@ -1,32 +1,61 @@
+import json
+import os
 import logging
 import glum
 from colorama import Fore, Style, init
 
-logging.basicConfig(level=logging.INFO)
+# Konstanty
+PROMPT_CHARACTER = f"Zadejte postavu"
+PROMPT_SEND = ", nebo 'send' pro odeslání konverzace: "
+
+# Inicializace
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 init()  # Inicializace colorama
+
+# Konstanty
+CONVERSATION_FILE = "conversation.json"
+
+def save_conversation(messages):
+    with open(CONVERSATION_FILE, "w") as file:
+        json.dump(messages, file, indent=2)
+
+def load_conversation():
+    if os.path.exists(CONVERSATION_FILE):
+        with open(CONVERSATION_FILE, "r") as file:
+            return json.load(file)
+    return [{"role": "system", "content": "Jsi Glum, postava z knihy Pán prstenů. Používej jeho repliky, aby tvé odpovědi byly co nejvíce v jeho stylu. Používej hodně slova 'my', 'Glum', 'můj a 'milášek'."}]  # Výchozí zpráva
+
+def get_characters():
+    characters = input("Zadejte postavy, oddělené čárkou: ").split(",")
+    return [char.strip() for char in characters]
+
+def get_user_input(characters):
+    character = input(f"{PROMPT_CHARACTER} ({', '.join(characters)}){PROMPT_SEND}")
+    if character.lower() in ['quit', 'send']:
+        return character.lower(), ""
+    if character not in characters:
+        print(f"{Fore.RED}Neznámá postava. Zkuste to znovu.{Style.RESET_ALL}")
+        return None, None  # Vrátí None, aby hlavní smyčka pokračovala
+    user_input = input(f"{Fore.MAGENTA}{character}: {Style.RESET_ALL}")
+    return character, user_input
 
 def main():
     print(Fore.CYAN + "Vítejte v chatbotu. Zadejte 'quit' pro ukončení." + Style.RESET_ALL)
-    characters = input("Zadejte postavy, oddělené čárkou: ").split(",")
-    characters = [char.strip() for char in characters]
-    messages = [{"role": "system", "content": "Jsi Glum, postava z knihy Pán prstenů. Používej jeho repliky, aby tvé odpovědi byly co nejvíce v jeho stylu. Používej hodně slova 'my', 'Glum', 'můj a 'milášek'."}]
-    print()  # Prázdný řádek pro lepší orientaci
+    characters = get_characters()
+    messages = load_conversation()
 
     while True:
-        character = input(f"Zadejte postavu ({', '.join(characters)}), nebo 'send' pro odeslání konverzace: ")
-        if character.lower() == 'quit':
+        print()  # Prázdný řádek pro lepší orientaci
+        character, user_input = get_user_input(characters)
+        if character == 'quit':
             break
-        if character.lower() == 'send':
+        if character == 'send':
             response = glum.get_response(messages)
-            logging.info(f'Received: {response}')
-            print(f'{Fore.GREEN}Glum: {response}{Style.RESET_ALL}\n')  # Prázdný řádek pro lepší orientaci
-            continue  # Skočí zpět na začátek smyčky
-        if character not in characters:
-            print(f"{Fore.RED}Neznámá postava. Zkuste to znovu.{Style.RESET_ALL}")
+            print(f'{Fore.GREEN}{response}{Style.RESET_ALL}\n')
+            save_conversation(messages)  # Uloží konverzaci po každém odeslání
             continue
-        user_input = input(f"{Fore.MAGENTA}{character}: {Style.RESET_ALL}")
-        messages.append({"role": "user", "content": f"{character}: {user_input}"})
-        logging.info(f'Sending: {messages[-1]}')
+        if character and user_input:  # Pokud není None
+            messages.append({"role": "user", "content": f"{character}: {user_input}"})
 
 if __name__ == "__main__":
     main()
